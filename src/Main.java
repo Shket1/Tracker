c4d66496e72bfddaf89428e0a54dc6bc62b24085
Alex Romanovskiy <ma4e94@gmail.com>
2024-01-20 23:35:54 +0300

    Добавил функцию установки цели шагов в день

--- src/Main.java
+++ src/Main.java
@@ -33,13 +33,14 @@ public class Main {
 
                 myTracker.saveProgress(month, day, steps);
                 System.out.println("Прогресс сохранен.");
+                System.out.println("______________________________________________________________");
             } else if (chose == 2) {  //здесь выводится количество шагов за день
                 System.out.println("Введите номер месяца от 1 до 12");
                 int month = sc.nextInt();
                 while (month > 12 || month < 1) {
                     System.out.println("Нету такого месяца. Попробуйте еще!");
                     month = sc.nextInt();
-                }
+                }System.out.println("______________________________________________________________");
 
                 System.out.println("Введите день от 1 до 30.");
                 int day = sc.nextInt();
@@ -49,6 +50,7 @@ public class Main {
                 }
 
                 myTracker.printDataStepsPerDay(day, month);
+                System.out.println("______________________________________________________________");
 
             } else if (chose == 3) {
                 System.out.println("Введите номер месяца от 1 до 12");
@@ -57,8 +59,8 @@ public class Main {
                     System.out.println("Нету такого месяца. Попробуйте еще!");
                     month = sc.nextInt();
                 }
-
                 myTracker.printAllStepsPerMonth(month);
+                System.out.println("______________________________________________________________");
             } else if (chose == 4) {
                 System.out.println("Введите номер месяца от 1 до 12");
                 int month = sc.nextInt();
@@ -68,6 +70,17 @@ public class Main {
                 }
 
                 myTracker.printStatisticPerMonth(month);
+                System.out.println("______________________________________________________________");
+            } else if (chose == 5) {
+                System.out.println("Введите желаемую цель.");
+                int target = sc.nextInt();
+                while (target < 0) {
+                    System.out.println("Цель не может быть меньше 1");
+                    target = sc.nextInt();
+                }
+                myTracker.dailyTarget = target;
+                System.out.println("Целевое количество шагов в сутки установлено на: " + target);
+                System.out.println("______________________________________________________________");
             }
             printMenu();
             chose = sc.nextInt();
@@ -81,6 +94,7 @@ public class Main {
         System.out.println("2 - Количество шагов за день");
         System.out.println("3 - Количество шагов за месяц");
         System.out.println("4 - Статистика за месяц");
+        System.out.println("5 - Установить цель шагов в день");
         System.out.println("0 - Выход");
     }
 }
